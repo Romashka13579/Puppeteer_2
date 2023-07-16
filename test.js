b66db4037c8e60ceb74c3d3984ef09de001ece10
Romashka <rroma13579@gmail.com>
2023-07-16 22:12:18 +0000

    testing commands for excel

--- test.js
+++ test.js
@@ -1,24 +1,24 @@
-const ExcelJS = require('exceljs');
+// const ExcelJS = require('exceljs');
 
-try {
-    const workbook = new ExcelJS.Workbook();
-    const worksheet = workbook.addWorksheet('Sheet 1');
-    worksheet.state = 'visible';
-    const cell = worksheet.getCell('A1'); 
+// try {
+//     const workbook = new ExcelJS.Workbook();
+//     const worksheet = workbook.addWorksheet('Sheet 1');
+//     worksheet.state = 'visible';
+//     const cell = worksheet.getCell('A1'); 
     
-    cell.value = 'Updated Value';
-    workbook.xlsx.writeFile('scraped_data.xlsx')
-      .then(() => {
-        console.log('Data saved to Excel file.');
-      })
-      .catch((error) => {
-        console.error('Error saving data to Excel file:', error);
-      });
+//     cell.value = 'Updated Value';
+//     workbook.xlsx.writeFile('scraped_data.xlsx')
+//       .then(() => {
+//         console.log('Data saved to Excel file.');
+//       })
+//       .catch((error) => {
+//         console.error('Error saving data to Excel file:', error);
+//       });
 
-}
-catch (error) {
-    console.error('Error opening Excel file:', error);
-}
+// }
+// catch (error) {
+//     console.error('Error opening Excel file:', error);
+// }
 
 // function myLoop() {
 //     setTimeout(function () {
@@ -53,3 +53,21 @@ catch (error) {
 // }
 
 // myLoop();
+
+
+const ExcelJS = require('exceljs');
+
+// const workbook = new ExcelJS.Workbook();
+// workbook.xlsx.readFile("Scraper.xlsx");
+const workbook = new ExcelJS.Workbook();
+
+const worksheet = workbook.addWorksheet('sheet', {properties:{tabColor:{argb:'FF00FF00'}}});
+
+worksheet.properties.defaultRowHeight = 50;
+worksheet.properties.defaultColWidth = 10;
+
+worksheet.getCell('A4').value = 'Four';
+
+workbook.xlsx.writeFile("scraped_data_test.xlsx");
+// workbook.xlsx.writeFile("Scraper.xlsx");
+
